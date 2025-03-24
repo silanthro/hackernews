c9f8b055345e9683f677c1fde8ef4b6e4365d351
SK <limsweekiat@gmail.com>
2025-03-24 20:53:46 +0800

    Switch from grequests to requests and remove search

--- tools.py
+++ tools.py
@@ -1,7 +1,7 @@
+import concurrent.futures as futures
 from typing import Literal, TypedDict
 
-import grequests
-from thefuzz import fuzz
+import requests
 
 BASE_URL = "https://hacker-news.firebaseio.com/v0"
 
@@ -13,6 +13,12 @@ class Post(TypedDict):
     by: str
     score: int
     timestamp: int
+
+
+def concurrent_requests(urls: list[str]):
+    with futures.ThreadPoolExecutor() as pool:
+        results = pool.map(lambda x: requests.get(x).json(), urls)
+    return results
 
 
 def get_stories(num: int = 10, key=Literal["topstories", "newstories"]) -> list[Post]:
@@ -33,12 +39,11 @@ def get_stories(num: int = 10, key=Literal["topstories", "newstories"]) -> list[
         - score (int): The current score of the story
         - timestamp (int): Creation timestamp of the story in Unix Time
     """
-    response = grequests.map((grequests.get(BASE_URL + f"/{key}.json"),))
-    data = response[0].json()
-    post_responses = grequests.map(
-        grequests.get(f"{BASE_URL}/item/{post_id}.json") for post_id in data[:num]
+    response = requests.get(BASE_URL + f"/{key}.json")
+    data = response.json()
+    posts = concurrent_requests(
+        [f"{BASE_URL}/item/{post_id}.json" for post_id in data[:num]]
     )
-    posts = [r.json() for r in post_responses]
     return [
         Post(
             title=p.get("title"),
@@ -88,26 +93,3 @@ def get_new_stories(num: int = 10) -> list[Post]:
         - timestamp (int): Creation timestamp of the story in Unix Time
     """
     return get_stories(num, "newstories")
-
-
-def search_new_stories_by_title(query: str, num: int = 10) -> list[Post]:
-    """
-    Search the newest stories by their titles
-    Uses fuzzy string matching to match the query to story titles
-
-    Args:
-    - query (str): Search query
-    - num (int): Number of stories to retrieve
-
-    Returns:
-        A list of stories, where each story is a dictionary with the following keys:
-        - title (str): The title of the story
-        - url (str): The URL of the posted story
-        - comments_url (str): The URL linking to the HackerNews comments thread
-        - by (str): The username of the original poster
-        - score (int): The current score of the story
-        - timestamp (int): Creation timestamp of the story in Unix Time
-    """
-    posts = get_new_stories(num=100)
-    posts.sort(key=lambda x: -fuzz.token_set_ratio(query, x["title"]))
-    return posts[:num]
